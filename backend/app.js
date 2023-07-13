faa706d1c54ead96d1027cd7e65968c565abec36
Razan <razan.ashhab.00@gmail.com>
2023-07-13 19:38:33 +0300

    save changes

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const {
-  NOT_FOUND,
-  BAD_REQUEST,
-  NOT_AUTHORIZED,
-  DEFAULT,
+    NOT_FOUND,
+    BAD_REQUEST,
+    NOT_AUTHORIZED,
+    DEFAULT,
 } = require("./utils/utils");
 
 const { PORT = 3000 } = process.env;
 const app = express();
 const auth = require("./middleware/auth");
 const { errors } = require("celebrate");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { requestLogger, errorLogger } = require("./middleware/logger");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,7 +27,7 @@ app.use("/", require("./routes/users"));
 app.use("/", require("./routes/cards"));
 
 app.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
+    res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
 app.use(errorLogger);
@@ -35,18 +35,18 @@ app.use(errorLogger);
 app.use(errors());
 
 app.use((err, req, res, next) => {
-  if (err.statusCode === NOT_FOUND)
-    res.status(NOT_FOUND).send({ message: err.message });
-  else if (err.name === "ValidationError")
-    res.status(BAD_REQUEST).send({ message: err.message });
-  else if (err.name === "CastError")
-    res.status(BAD_REQUEST).send({ message: err.message });
-  else
-    res
-      .status(DEFAULT)
-      .send({ message: "An error has occurred on the server" });
+    if (err.statusCode === NOT_FOUND)
+        res.status(NOT_FOUND).send({ message: err.message });
+    else if (err.name === "ValidationError")
+        res.status(BAD_REQUEST).send({ message: err.message });
+    else if (err.name === "CastError")
+        res.status(BAD_REQUEST).send({ message: err.message });
+    else
+        res
+        .status(DEFAULT)
+        .send({ message: "An error has occurred on the server" });
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`);
+    console.log(`App listening at port ${PORT}`);
 });
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,42 +1,86 @@
 const router = require("express").Router();
 const {
-  getUsers,
-  getUser,
-  createUser,
-  updateProfile,
-  updateAvatar,
-  getUserByToken,
-  login,
+    getUsers,
+    getUser,
+    createUser,
+    updateProfile,
+    updateAvatar,
+    getUserByToken,
+    login,
 } = require("../controllers/users");
 const { celebrate, Joi } = require("celebrate");
 const { validateURL } = require("../utils/validator");
 
 router.get("/users", getUsers);
 
-router.get("/users/:userId", getUser);
-
-router.post(
-  "/users",
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
-      avatar: Joi.string().custom(validateURL),
+router.get(
+    "/users/:userId",
+    celebrate({
+        params: Joi.object().keys({
+            postId: Joi.string().alphanum().length(24),
+        }),
     }),
-  }),
-  createUser
+    getUser
 );
 
-router.patch("/users/me", updateProfile);
+router.post(
+    "/users",
+    celebrate({
+        body: Joi.object().keys({
+            email: Joi.string().required().email(),
+            password: Joi.string().required().min(8),
+            name: Joi.string().required().min(2).max(30),
+            about: Joi.string().required().min(2).max(30),
+            avatar: Joi.string().custom(validateURL),
+        }),
+    }),
+    createUser
+);
 
-router.patch("/users/me/avatar", updateAvatar);
+router.patch(
+    "/users/me",
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required().min(2).max(30),
+            about: Joi.string().required().min(2).max(30),
+            avatar: Joi.string().custom(validateURL),
+        }),
+    }),
+    updateProfile
+);
+
+router.patch(
+    "/users/me/avatar",
+    celebrate({
+        body: Joi.object().keys({
+            avatar: Joi.string().custom(validateURL),
+        }),
+    }),
+    updateAvatar
+);
 
 router.get("/users/me", getUserByToken);
 
-router.post("/signin", login);
+router.post(
+    "/signin",
+    celebrate({
+        body: Joi.object().keys({
+            email: Joi.string().required().email(),
+            password: Joi.string().required().min(8),
+        }),
+    }),
+    login
+);
 
-router.post("/signup", createUser);
+router.post(
+    "/signup",
+    celebrate({
+        body: Joi.object().keys({
+            email: Joi.string().required().email(),
+            password: Joi.string().required().min(8),
+        }),
+    }),
+    createUser
+);
 
 module.exports = router;
