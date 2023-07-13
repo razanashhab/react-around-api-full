423d1601410a45831601ca22d589822a814cadd6
Razan <razan.ashhab.00@gmail.com>
2023-07-13 23:06:14 +0300

    Save CHanges

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
     const { authorization } = req.headers;
 
     if (!authorization || !authorization.startsWith("Bearer ")) {
-        return res.status(401).send({ message: "Authorization Required" });
+        return res.status(401).send({ message: "Authorization is Required" });
     }
 
     const token = authorization.replace("Bearer ", "");
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
     try {
         payload = jwt.verify(token, "super-strong-secret");
     } catch (err) {
-        return res.status(401).send({ message: "Authorization Required" });
+        return res.status(401).send({ message: "Authorization is Required" });
     }
 
     req.user = payload;
